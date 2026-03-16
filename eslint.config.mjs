d477dc00a4d9a6fe81e368647348fa3d307ae514
Oksana Zalivan <ksyushamartinovska@gmail.com>
2026-03-16 17:45:23 +0300

    init express server for notes app

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'commonjs',
+    },
+
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
+];
