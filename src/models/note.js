c66c3bc83456cc4c440c8c5f9d5fc8c3a165c4e7
Oksana Zalivan <ksyushamartinovska@gmail.com>
2026-03-25 15:45:27 +0300

    fix: adjust controllers and model to meet requirements

--- src/models/note.js
+++ src/models/note.js
@@ -1,15 +1,14 @@
-import { Schema, model } from 'mongoose';
-import { TAGS } from '../constants/tags.js';
-
 const noteSchema = new Schema(
   {
     title: {
       type: String,
       required: true,
+      trim: true,
     },
     content: {
       type: String,
       default: '',
+      trim: true,
     },
     tag: {
       type: String,
@@ -22,7 +21,3 @@ const noteSchema = new Schema(
     versionKey: false,
   },
 );
-
-noteSchema.index({ title: 'text', content: 'text' });
-
-export const Note = model('Note', noteSchema);
